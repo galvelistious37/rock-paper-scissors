1b0f6754337e97838a744617583ee253b0c6995d
galvelistious37 <galvelistious37@gmail.com>
2021-09-01 23:53:25 -0700

    script.js fuctionality is pretty much done. Still need to refactor and add comments/doco

--- script.js
+++ script.js
@@ -3,12 +3,86 @@ const rock = "Rock"
 const paper = "Paper"
 const scissors = "Scissors"
 
+// instantiate global variables
+let round = 1
+let playerScore = 0;
+let computerScore = 0;
+
 // Instantiate array
 let choices = [rock, paper, scissors];
+
+// Main game function to keep track of rounds
+function game(){
+    for(let i = 1; i < 6; i++){
+        console.log(playRound());
+    }
+}
+
+// Function to play a single round. Take player selection and
+// computerPlay value as parameters to determine winner
+function playRound(){
+    let playerSelection = getPlayerSelection();
+    let computerSelection = computerPlay();
+    if(playerSelection === computerSelection){
+        return "Draw! You both picked " + playerSelection;
+    } else {
+        let winner = getWinner(playerSelection, computerSelection);
+    return winner ? "You Won! " + playerSelection + " beats " + computerSelection :
+        "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
+function getWinner(playerSelection, computerSelection){
+    let playerValue = choices.indexOf(playerSelection);
+    let computerValue = choices.indexOf(computerSelection);
+    if(playerSelection == rock && computerSelection == scissors){
+        playerValue += 3;
+    }
+    if (computerSelection == rock && playerSelection == scissors){
+        computerValue =+3;
+    }
+    return playerValue > computerValue;
+}
+
+// Prompt the user and return input
+function getPlayerSelection(){
+    let keepGoing = true;
+    let promptChoices = ` ${choices[0]}, ${choices[1]}, or ${choices[2]}`;
+    let promptText = `Choose One: ${promptChoices}`;
+    let invalidChoice = `Invalid Selection - You must choose: ${promptChoices}`;
+    let input = formattedInput(prompt(`${promptText}`));
+    while(keepGoing){
+        if(!isInputValid(input)){
+            input = formattedInput(prompt(`${invalidChoice}`));
+        } else {
+            keepGoing = false;
+        } 
+    }
+    input = formattedInput(input);
+    return input;
+}
+
+// Verify the user input is valid
+function isInputValid(input){
+    return (input == choices[0] ||
+            input == choices[1] ||
+            input == choices[2]);
+}
 
 // Get a radomly selected value for computer's choice
 function computerPlay(){
     return getArrayValueOfIndex(getRandomNumber());
+}
+
+// Returns a formatted input with a capitalized first letter and 
+// the rest of the word in lowercase.
+function formattedInput(input){
+    let charArr = input.split("");
+    charArr[0] = charArr[0].toUpperCase();
+    for(let i = 1; i < charArr.length; i++){
+        charArr[i] = charArr[i].toLowerCase();
+    }
+    return charArr.join("");
 }
 
 // return a random integer between 0 - 2
@@ -21,7 +95,4 @@ function getArrayValueOfIndex(index){
     return choices[index];
 }
 
-let compSelection = computerPlay();
-document.getElementById('computerChoice').innerText = compSelection
-console.log(compSelection)
-
+game()
